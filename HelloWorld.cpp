1444c495673d134b8462dfbe769789ace5777bc4
rehloy <79176541+rehloy@users.noreply.github.com>
2021-02-17 19:55:32 +0100

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	//Ausgabe des Textes Hello World! auf der Konsole
+
+	cout << "Hello World!\n";
+	cout << endl;
+
+	system("pause");
+
+	return 0;
+}
